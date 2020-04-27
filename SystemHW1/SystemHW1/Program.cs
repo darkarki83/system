09f19299610045a812e100b15624c4794dacac9b
Artem Krol <artemkrol47@gmail.com>
2020-04-27 11:15:26 +0300

    system first + second

---
+++ SystemHW1/SystemHW1/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+
+namespace SystemHW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Artem Krol"))
+                {
+                    if (key != null)
+                    {
+                        object o = key.GetValue("Title");
+                        Console.WriteLine(o.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            try
+            {
+                using (RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run"))
+                {
+                    key.SetValue("Artem Krol Super Prog", $"{ Process.GetCurrentProcess().MainModule.FileName}");
+                
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                using (RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Software\Artem Krol"))
+                {
+                    key.SetValue("Title", $"{DateTime.Now:dd.MM.yyyy HH:mm:ss}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ SystemHW2/SystemHW2/Program.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+
+namespace SystemHW2
+{
+    class Program
+    {
+       // public string KeyName = @"Software\Artem Krol\MyApp\RecentOpenedFiles";
+        static void SaveList(List<string> list)
+        {
+            try
+            {
+                using(RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Software\Artem Krol\MyFiles"))
+                {
+                    int count = 1;
+                    foreach (var item in list)
+                        key.SetValue($"str{count++}", item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static List<string> InputList()
+        {
+            var list = new List<string>();
+            try
+            {
+                using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Artem Krol\MyFiles"))
+                {
+                    foreach (var item in key.GetValueNames())
+                        list.Add(key.GetValue(item).ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return list;
+        }
+
+        static void Main(string[] args)
+        {
+            SaveList(new List<string>()
+        {
+            @"C:\Docs\Report1.docx",
+            @"C:\Docs\Report2.docx",
+            @"C:\Docs\Report3.docx",
+            @"C:\Docs\Report4.docx",
+            @"C:\Docs\Report5.docx",
+            @"C:\Docs\Report56.docx",
+            @"C:\Docs\Report54.docx",
+            @"C:\Docs\Report53.docx",
+            @"C:\Docs\Report52.docx",
+            @"C:\Docs\Report51.docx",
+            @"C:\Docs\Report9.docx"
+        });
+
+            List<string> list = InputList();
+            foreach (var item in list)
+                Console.WriteLine(item);
+        }
+    }
+}
