b24fa64fe39ffeb42900a915c04de303b9472c6f
Artem Krol <artemkrol47@gmail.com>
2020-05-25 12:28:17 +0300

    puh +1

---
+++ Balloons/Balloons/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+
+namespace Balloons
+{
+
+    class Resources
+    {
+        public static void WriteChar(int _x, int _y, char _symbol)
+        {
+            Console.SetCursorPosition(_x, _y);
+            Console.Write(_symbol);
+        }
+    }
+
+    class ThreadBallons
+    {
+        private Random random = new Random();
+        private char _symbol;
+        private static object pothisionLock = new object();
+        public Thread Thread { get; }
+        public int Hight { get; set; }
+        public int Width { get; set; }
+
+
+        public ThreadBallons(char symbol)
+        {
+            _symbol = symbol;
+            Thread = new Thread(Run);
+            Thread.Start();
+        }
+
+        // Точка входа в поток (функция потока)
+        void Run()
+        {
+            Hight = random.Next(1, 31);
+            Width = random.Next(1, 81);
+            do
+            {
+                if(Hight < 20)
+                {
+                    lock (pothisionLock)
+                    {
+                        Resources.WriteChar(Width, Hight, _symbol);
+                    }
+                }
+                Thread.Sleep(350);
+
+                lock (pothisionLock)
+                {
+                    Resources.WriteChar(Width, Hight, ' ');
+                }
+                Hight = random.Next(1, 31);
+                Width = random.Next(1, 81);
+            } while (Hight < 20);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                var ThreadBallons = new ThreadBallons('B');
+            }
+            
+            while(true)
+            { }
+        }
+    }
+}
---
+++ Synchro1/Synchro1/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Threading;
+
+namespace Synchro1
+{
+    class Resources
+    {
+        // Собственно, общие данные для потоков.
+        // Учитывая static, по сути, это глобальная переменная,
+        // т.е., как раз то, доступ чему нуждается в синхронизации.
+        public static int Honey { get; set; } = 0;
+
+        // Мьютекс для синхронизации потоков
+        public static Mutex Mutex { get; } = new Mutex();
+    }
+
+    class ThreadBee
+    {
+        private Random random = new Random();
+
+        public Thread Thread { get; }
+
+        public ThreadBee(string name)
+        {
+            Thread = new Thread(Run);
+            Thread.Name = name;
+            Thread.Start();
+        }
+
+        // Точка входа в поток (функция потока)
+        void Run()
+        {
+            while (true)
+            {
+                int time = random.Next(1, 6) * 1000;
+                Thread.Sleep(time);
+
+                Console.WriteLine($"{Thread.Name} ожидает мьютекс");
+                Resources.Mutex.WaitOne();
+                Console.WriteLine($"{Thread.Name} получает мьютекс");
+                Resources.Honey++;
+                Console.WriteLine($"{Thread.Name} освобождает мьютекс");
+                Resources.Mutex.ReleaseMutex();
+            }
+        }
+    }
+
+    class ThreadBeer
+    {
+        public Thread Thread { get; }
+
+        public ThreadBeer(string name)
+        {
+            Thread = new Thread(Run);
+            Thread.Name = name;
+            Thread.Start();
+        }
+
+        // Точка входа в поток (функция потока)
+        void Run()
+        {
+            while (true)
+            {
+                Thread.Sleep(10000);
+
+                Console.WriteLine($"{Thread.Name} ожидает мьютекс");
+                Resources.Mutex.WaitOne();
+                Console.WriteLine($"{Thread.Name} получает мьютекс");
+                Resources.Honey -= 15;
+                Console.WriteLine($"{Thread.Name} освобождает мьютекс");
+                Resources.Mutex.ReleaseMutex();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                var ThreadBee = new ThreadBee(i.ToString());
+            }
+
+            // Стартуем инкрементирующий поток
+            // (перед стартом декрементирующего потока делаем паузу в 1 мс; этого
+            // достаточно, чтобы инкрементирующий поток успел захватить мьютекс первым)
+            Thread.Sleep(1000);
+
+            
+            var ThreadBeer = new ThreadBeer("Beer Eat");
+
+            while (true)
+            {
+                Resources.Mutex.WaitOne();
+                if (Resources.Honey < 0)
+                    Console.WriteLine("Beer is Dead");
+                else
+                    Console.WriteLine($"Beer is Live. I have Honey = {Resources.Honey}");
+                Resources.Mutex.ReleaseMutex();
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
