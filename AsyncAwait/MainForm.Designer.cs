6c34ff5f1b8d8e9aba8e8998209c2085c533ce4d
Artem Krol <artemkrol47@gmail.com>
2020-06-18 09:41:31 +0300

    async-await

---
+++ AsyncAwait/MainForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace AsyncAwait
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelFrom = new System.Windows.Forms.Label();
+            this.labelTo = new System.Windows.Forms.Label();
+            this.textBoxFrom = new System.Windows.Forms.TextBox();
+            this.textBoxTo = new System.Windows.Forms.TextBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.buttonFromFile = new System.Windows.Forms.Button();
+            this.buttonToFile = new System.Windows.Forms.Button();
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // labelFrom
+            // 
+            this.labelFrom.AutoSize = true;
+            this.labelFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelFrom.Location = new System.Drawing.Point(29, 37);
+            this.labelFrom.Name = "labelFrom";
+            this.labelFrom.Size = new System.Drawing.Size(55, 16);
+            this.labelFrom.TabIndex = 0;
+            this.labelFrom.Text = "From : ";
+            // 
+            // labelTo
+            // 
+            this.labelTo.AutoSize = true;
+            this.labelTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTo.Location = new System.Drawing.Point(29, 83);
+            this.labelTo.Name = "labelTo";
+            this.labelTo.Size = new System.Drawing.Size(39, 16);
+            this.labelTo.TabIndex = 1;
+            this.labelTo.Text = "To : ";
+            // 
+            // textBoxFrom
+            // 
+            this.textBoxFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxFrom.Location = new System.Drawing.Point(109, 37);
+            this.textBoxFrom.Name = "textBoxFrom";
+            this.textBoxFrom.Size = new System.Drawing.Size(421, 22);
+            this.textBoxFrom.TabIndex = 2;
+            this.textBoxFrom.TextChanged += new System.EventHandler(this.textBoxFrom_TextChanged);
+            // 
+            // textBoxTo
+            // 
+            this.textBoxTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxTo.Location = new System.Drawing.Point(109, 83);
+            this.textBoxTo.Name = "textBoxTo";
+            this.textBoxTo.Size = new System.Drawing.Size(421, 22);
+            this.textBoxTo.TabIndex = 3;
+            this.textBoxTo.TextChanged += new System.EventHandler(this.textBoxFrom_TextChanged);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(32, 137);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(498, 23);
+            this.progressBar.TabIndex = 4;
+            // 
+            // buttonFromFile
+            // 
+            this.buttonFromFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonFromFile.Location = new System.Drawing.Point(548, 37);
+            this.buttonFromFile.Name = "buttonFromFile";
+            this.buttonFromFile.Size = new System.Drawing.Size(92, 23);
+            this.buttonFromFile.TabIndex = 5;
+            this.buttonFromFile.Text = "File";
+            this.buttonFromFile.UseVisualStyleBackColor = true;
+            this.buttonFromFile.Click += new System.EventHandler(this.buttonFromFile_Click);
+            // 
+            // buttonToFile
+            // 
+            this.buttonToFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonToFile.Location = new System.Drawing.Point(548, 83);
+            this.buttonToFile.Name = "buttonToFile";
+            this.buttonToFile.Size = new System.Drawing.Size(92, 23);
+            this.buttonToFile.TabIndex = 6;
+            this.buttonToFile.Text = "File";
+            this.buttonToFile.UseVisualStyleBackColor = true;
+            this.buttonToFile.Click += new System.EventHandler(this.buttonToFile_Click);
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCopy.Location = new System.Drawing.Point(548, 137);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(92, 23);
+            this.buttonCopy.TabIndex = 7;
+            this.buttonCopy.Text = "Copy";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(32, 182);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(608, 264);
+            this.listBox1.TabIndex = 8;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 461);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonCopy);
+            this.Controls.Add(this.buttonToFile);
+            this.Controls.Add(this.buttonFromFile);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.textBoxTo);
+            this.Controls.Add(this.textBoxFrom);
+            this.Controls.Add(this.labelTo);
+            this.Controls.Add(this.labelFrom);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "CopyFiles";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelFrom;
+        private System.Windows.Forms.Label labelTo;
+        private System.Windows.Forms.TextBox textBoxFrom;
+        private System.Windows.Forms.TextBox textBoxTo;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button buttonFromFile;
+        private System.Windows.Forms.Button buttonToFile;
+        private System.Windows.Forms.Button buttonCopy;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ AsyncAwait/MainForm.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAwait
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            buttonCopy.Enabled = false;
+        }
+
+        private void buttonFromFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var fileDialog = new OpenFileDialog();
+                fileDialog.Filter = "Text files (*.txt)|*.txt|C++ files (*.cpp;*.h;*.hpp)|*.cpp;*.h;*.hpp|All files (*.*)|*.*";
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBoxFrom.Text = fileDialog.FileName;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Can not open File " + ex.Message, "File Open", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void buttonToFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var fileDialog = new OpenFileDialog();
+                fileDialog.Filter = "Text files (*.txt)|*.txt|C++ files (*.cpp;*.h;*.hpp)|*.cpp;*.h;*.hpp|All files (*.*)|*.*";
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBoxTo.Text = fileDialog.FileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open File " + ex.Message, "File Open", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private async void buttonCopy_Click(object sender, EventArgs e)
+        {
+           
+            // Создаем объект класса System.Progress. Именно в нем мы обращаемся к UI.
+            var progress = new Progress<Tuple<int>>(t =>
+            {
+                progressBar.Value = t.Item1;
+            });
+            // Запускаем асинхронную операцию
+            await Task.Factory.StartNew(() => SomeLongOperation(progress), TaskCreationOptions.LongRunning);
+
+        }
+
+        private void textBoxFrom_TextChanged(object sender, EventArgs e)
+        {
+            if(textBoxFrom.TextLength > 0 && textBoxTo.TextLength > 0)
+            {
+                buttonCopy.Enabled = true;
+            }
+        }
+
+        private async void SomeLongOperation(IProgress<Tuple<int>> progress)
+        {
+            // ВАЖНО: Здесь никогда не обращайтесь к UI (т.е., к форме и ее контролам)
+            string textFromFile = "";
+            using (FileStream fstream = File.OpenRead($"{textBoxFrom.Text}"))
+            {
+                byte[] array = new byte[fstream.Length];
+                // асинхронное чтение файла
+                await fstream.ReadAsync(array, 0, array.Length);
+
+                textFromFile = System.Text.Encoding.Default.GetString(array);
+                //Console.WriteLine($"Текст из файла: {textFromFile}");
+                /*int i = 0;
+                string temp = "";
+                foreach (var item in textFromFile)
+                {
+                    temp += item;
+                    i++;
+                    if (item == '\n')
+                    {
+
+                        listBox1.Items.Add(temp);
+                        temp = "";
+
+                    }
+                }
+                listBox1.Items.Add(temp);*/ // на посмотреть
+            }
+            string files = string.Empty;
+            textBoxTo.Invoke((MethodInvoker)(() => files = textBoxTo.Text));
+
+            using (FileStream fstream2 = new FileStream(textBoxTo.Text, FileMode.OpenOrCreate))
+            {
+                byte[] array = Encoding.Default.GetBytes(textFromFile);
+                int count = Encoding.Default.GetByteCount(textFromFile);
+                int k = count / 100;
+                for (int i = 0; i < 100; i++)
+                {
+
+                    Task.Delay(100).Wait();
+                    if (i != 99)
+                    {
+                        fstream2.Write(array, i * k, k * (i + 1));
+                    }
+                    else
+                    {
+                        fstream2.Write(array, i * k, count);
+                    }
+                    progress.Report(new Tuple<int>(i));
+                }
+                //fstream2.Write(array, 0, 144);
+                // асинхронная запись массива байтов в файл
+                //await fstream2.WriteAsync(array, 0, array.Length);
+        
+            }
+            // Выполняем длительную операцию...
+           // for (int i = 1; i <= 100; i++)
+            {
+                // Здесь в боевых проектах мы будем делать какую-то полезную работу
+                //Task.Delay(100).Wait();
+
+                // Сообщаем о необходимости обновить текст и состояние полосы прогресса
+                //progress.Report(new Tuple<int>(i));
+
+            }
+            // Сообщаем о необходимости обновить текст и состояние полосы
+            // прогресса с информацией о 100% завершении работы
+            //progress.Report(new Tuple<int>(100));
+        }
+    }
+}
